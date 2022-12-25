f4f65685ce90d47897a35542429c6d49eb9acaa8
doublevkay <vovikhangcdv@gmail.com>
2022-12-25 21:45:48 +0700

    big change

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const secret = process.env.SECRET;
-const FLAG = process.env.FLAG;
-/* GET home page. */
+require('dotenv').config();
+const SECRET = process.env.SECRET;
+const REPO = process.env.REPO;
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { });
 });
 
-router.get('/2023', function(req, res, next) {
-  res.render('2023', { title: 'Express' });
+router.get('/credit', function(req, res, next) {
+  const fs = require('fs');
+  const usernames = fs.readFileSync('credits.txt', 'utf8').split('\n');
+  const users = {};
+  usernames.forEach(function(username) {
+    if (username ) users[username] = 1 + (users[username] || 0);
+  });
+  const leaderboard = Object.keys(users).sort(function(a, b) { return users[b] - users[a] });
+  res.render('credit', { users: users, leaderboard: leaderboard, repo: REPO});
+});
+
+router.post('/credit', function(req, res, next) {
+  if (!req.body.username || !req.body.secret || req.body.secret !== SECRET) {
+    res.send('Invalid username or secret');
+    return;
+  }
+  const username = req.body.username.toString();
+  const fs = require('fs');
+  fs.appendFileSync('credits.txt', username + "\n");
+  res.send('Credited!');
 });
 
 module.exports = router;
